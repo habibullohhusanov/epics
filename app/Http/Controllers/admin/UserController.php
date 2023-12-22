f834f64f926d752477a59c7c35cc58195fecaaa3
JavohirSolution <javohirdeveloper12@gmail.com>
2023-12-22 17:35:34 +0500

    create component

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::all();
+        return view('secret.dash', [
+            'users' => $user
+        ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,17 @@ use App\Http\Controllers\admin\ImageController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\auth\AuthController;
 use Illuminate\Support\Facades\Route;
-
+// Route::get('secret', function () {
+//     return view('secret/dash');
+// })->middleware('auth');
+// Route::redirect('/', 'secret')->middleware('auth');
+// Route::middleware('auth')->group(function () {
+//     Route::resources([
+//         'user' => UserController::class,
+//         'image' => ImageController::class,
+//         'find' => FindController::class,
+//     ]);
+// });
 Route::get('secret', function () {
     return view('secret/dash');
 });
