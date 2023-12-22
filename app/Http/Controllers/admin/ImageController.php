f1bb6ef102d800b054ba4b8145f1822ad42e24da
Habibulloh <husanovhabibullohhusanov@gmail.com>
2023-12-22 16:26:46 +0500

    Admin controllers added

--- app/Http/Controllers/admin/ImageController.php
+++ app/Http/Controllers/admin/ImageController.php
@@ -3,63 +3,28 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $images = Image::all()->paginate(20);
+        return view('secret.images', [
+            'images' => $images
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $image = Image::find($id);
+        return view('secret.image');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function destroy(Image $image)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $image->delete();
+        return redirect()->back()->with('success', "Image deleted");
     }
 }
